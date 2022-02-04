b270cbb44803233129261c05cb49826d36455924
MaksSan <64585283+MaksSan@users.noreply.github.com>
2022-02-04 22:07:50 +0300

    edit_comment

--- Collections/Collections.py
+++ Collections/Collections.py
@@ -14,11 +14,14 @@ for dictionary in rand_dict:                                                    
         tempr_dict.setdefault(k, []).append(v)                                  #assigning key values and adding
 
 #searching the biggest values for key
-for k, v in tempr_dict.items():                                                 #take the key value from the dict
-    if len(v) > 1:                                                              #condition for execution: if the number of values is more than one for key
-        my_dict[k+"_"+str(v.index(max(v))+1)] = max(v)                          #searching the biggest value for key
-    else:
-        my_dict[k] = v[0]                                                       #assigns a value to a key with a zero key
+try:
+    for k, v in tempr_dict.items():                                             #take the key value from the dict
+        if len(v) > 1:                                                          #condition for execution: if the number of values is more than one for key
+            my_dict[k+"_"+str(v.index(max(v))+1)] = max(v)                      #searching the biggest value for key
+        else:
+            my_dict[k] = v[0]                                                   #assigns a value to a key with a zero key
+except IndexError:
+    print(f'Index is out of range')
 
 print(f'Random dictionary: {rand_dict}')                                        #printing the random dictionary
 print(f'Result: {my_dict}')                                                     #printing the result

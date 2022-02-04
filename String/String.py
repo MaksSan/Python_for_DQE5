9eede38a226eb5a55e6e7af808e9e761d7f6d0a6
MaksSan <64585283+MaksSan@users.noreply.github.com>
2022-02-04 17:46:13 +0300

    version 1.0

--- String/String.py
+++ String/String.py
@@ -1,6 +1,38 @@
-import re
+original_text = '''homEwork:
+	tHis iz your homeWork, copy these Text to variable. 
 
-with open('text.txt', 'r') as file:
-    original_text = file.read()
-print(original_text)
+	You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph.
 
+	it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE. 
+
+	last iz TO calculate nuMber OF Whitespace characteRS in this Text. caREFULL, not only Spaces, but ALL whitespaces. I got 87.'''
+
+num_whitespace = 0
+some_variable = []
+strings = list()
+strings1 = list()
+
+for c in original_text:
+    if c in {' ', '\n', '\t'}:
+        num_whitespace += 1
+
+replaced_text = original_text.lower().replace(' iz ', ' is ').replace('\t', '').replace('\t', '').replace('\n', '')
+
+for x in replaced_text.split('.'):
+    some_variable.append(x)
+try:
+    for x in replaced_text.split('. '):
+        x = x.strip()
+        x = x.capitalize()
+        strings.append(x)
+        c = x.split()
+        strings1.append(c[-1])
+except IndexError:
+    print(f'Index is out of range')
+
+newline = '. \n'.join(strings) + '\n' + ' '.join(strings1).capitalize()
+
+print(f'=================================')
+print(f'Numbers of whitespaces: {num_whitespace}')
+print(f'=================================')
+print(f'Normalized text: \n{newline}')

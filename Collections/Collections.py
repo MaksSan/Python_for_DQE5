7d2724ca32c32f1eb577fe04776f06c5ee034a98
MaksSan <64585283+MaksSan@users.noreply.github.com>
2022-02-04 21:52:17 +0300

    upd3

--- Collections/Collections.py
+++ Collections/Collections.py
@@ -20,8 +20,8 @@ for k, v in tempr_dict.items():                                                 
     else:
         my_dict[k] = v[0]                                                       #assigns a value to a key with a zero key
 
-print("Random dictionary: " + str(rand_dict))                                   #printing the random dictionary
-print("Result: " + str(my_dict))                                                #printing the result
+print(f'Random dictionary: {rand_dict}')                                        #printing the random dictionary
+print(f'Result: {my_dict}')                                                     #printing the result
 
 
 
--- String/String.py
+++ String/String.py
@@ -1,7 +1,7 @@
 original_text = '''homEwork:
 	tHis iz your homeWork, copy these Text to variable. 
 
-	You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph.
+	You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph. 
 
 	it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE. 
 
@@ -12,21 +12,17 @@ some_variable = []
 strings = list()
 strings1 = list()
 
-for c in original_text:
+for c in original_text:                                                                                                 #counting the number of all whitespaces
     if c in {' ', '\n', '\t'}:
         num_whitespace += 1
 
-replaced_text = original_text.lower().replace(' iz ', ' is ').replace('\t', '').replace('\n', '')
+replaced_text = original_text.lower().replace(' iz ', ' is ').replace('\t', '').replace('\n', '')                       #replacements
 
-for x in replaced_text.split('.'):
-    some_variable.append(x)
 try:
-    for x in replaced_text.split('. '):
-        x = x.strip()
-        x = x.capitalize()
-        strings.append(x)
-        c = x.split()
-        strings1.append(c[-1])
+    for x in replaced_text.split('.'):                                                                                  #splitting all text by dot
+        if len(x) > 0:                                                                                                  #condition for not zero leng
+            strings.append(x.strip().capitalize())                                                                      #
+            strings1.append(x.split()[-1])
 except IndexError:
     print(f'Index is out of range')
 

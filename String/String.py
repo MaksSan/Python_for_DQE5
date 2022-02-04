224eec39f86326698c6f423acb40e8e5c6c9b056
MaksSan <64585283+MaksSan@users.noreply.github.com>
2022-02-04 22:03:21 +0300

    last und

--- String/String.py
+++ String/String.py
@@ -8,9 +8,8 @@ original_text = '''homEwork:
 	last iz TO calculate nuMber OF Whitespace characteRS in this Text. caREFULL, not only Spaces, but ALL whitespaces. I got 87.'''
 
 num_whitespace = 0
-some_variable = []
-strings = list()
-strings1 = list()
+general_text = list()
+collected_sentence = list()
 
 for c in original_text:                                                                                                 #counting the number of all whitespaces
     if c in {' ', '\n', '\t'}:
@@ -19,16 +18,16 @@ for c in original_text:                                                         
 replaced_text = original_text.lower().replace(' iz ', ' is ').replace('\t', '').replace('\n', '')                       #replacements
 
 try:
-    for x in replaced_text.split('.'):                                                                                  #splitting all text by dot
+    for x in replaced_text.split('. '):                                                                                  #splitting all text by dot
         if len(x) > 0:                                                                                                  #condition for not zero leng
-            strings.append(x.strip().capitalize())                                                                      #
-            strings1.append(x.split()[-1])
+            general_text.append(x.strip().capitalize())                                                                 #adding sentences/removing spaces at the begin/capital letler for fIrst words
+            collected_sentence.append(x.split()[-1])                                                                    #creating sentence with last words of each existing sentence
 except IndexError:
     print(f'Index is out of range')
 
-newline = '. \n'.join(strings) + '\n' + ' '.join(strings1).capitalize()
+new_text = '. \n'.join(general_text) + '\n' + ' '.join(collected_sentence).capitalize()                                 #joining general text with sentence from last words
 
-print(f'=================================')
+print(f'Normalized text: \n{new_text}')
+print(f'\n=================================')
 print(f'Numbers of whitespaces: {num_whitespace}')
 print(f'=================================')
-print(f'Normalized text: \n{newline}')

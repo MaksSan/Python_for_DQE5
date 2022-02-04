8eab9f9298e0bfbcfae1672adb66f21b2497f1f0
MaksSan <64585283+MaksSan@users.noreply.github.com>
2022-02-04 22:04:16 +0300

    edit_comment

--- String/String.py
+++ String/String.py
@@ -18,7 +18,7 @@ for c in original_text:                                                         
 replaced_text = original_text.lower().replace(' iz ', ' is ').replace('\t', '').replace('\n', '')                       #replacements
 
 try:
-    for x in replaced_text.split('. '):                                                                                  #splitting all text by dot
+    for x in replaced_text.split('. '):                                                                                 #splitting all text by dot
         if len(x) > 0:                                                                                                  #condition for not zero leng
             general_text.append(x.strip().capitalize())                                                                 #adding sentences/removing spaces at the begin/capital letler for fIrst words
             collected_sentence.append(x.split()[-1])                                                                    #creating sentence with last words of each existing sentence
